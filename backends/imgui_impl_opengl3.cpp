b9103cb9552f839cb605386b38da3a934abce2f7
purplemoonboi <purplemoonone@gmail.com>
2021-06-29 11:26:21 +0100

    Added ImGui callback events

--- backends/imgui_impl_opengl3.cpp
+++ backends/imgui_impl_opengl3.cpp
@@ -85,6 +85,7 @@
 
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
+#include "glad/glad.h"
 #include <stdio.h>
 #if defined(_MSC_VER) && _MSC_VER <= 1500 // MSVC 2008 or earlier
 #include <stddef.h>     // intptr_t
