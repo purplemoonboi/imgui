8c1c82638c1e04fd7f3385e5d446d59a4d12e419
purplemoonboi <purplemoonone@gmail.com>
2021-07-03 14:12:33 +0100

    Added docking to Foundation

--- premake5.lua
+++ premake5.lua
@@ -16,8 +16,14 @@ project "ImGui"
             "imstb_rectpack.h",
             "imstb_textedit.h",
             "imstb_truetype.h",
-            "imgui_demo.cpp"
+            "imgui_demo.cpp",
+	    "imgui_tables.cpp"
         }
+
+	defines
+	{
+	    "IMGUI_API=__declspec(dllexport)"
+	}
 
         filter "system:windows"
         systemversion "latest"
